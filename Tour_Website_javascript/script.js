b213ac569b2a4c98016d61a1b11f6158cec5b71b
Meet Karena <meetkarena619@gmail.com>
2026-02-07 16:53:25 +0530

    Add files via upload

---
+++ Tour_Website_javascript/script.js
@@ -0,0 +1,11 @@
+// QUICK INQUIRY BUTTON
+const quickInquiry = document.querySelector(".quick-inquiry");
+quickInquiry.addEventListener("click", function () {
+    alert("Quick Inquiry form opens here!");
+});
+
+// BOOK NOW CLICK
+const bookNowBtn = document.getElementById("bookNowBtn");
+bookNowBtn.addEventListener("click", function () {
+    alert("Redirect to booking page!");
+});
